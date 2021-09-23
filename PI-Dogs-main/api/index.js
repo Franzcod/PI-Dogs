77a7158a9350215bbbc65789d9355216720b1f09
Francisco Palacios <palacios.francisco@outlook.com>
2021-09-23 12:26:29 -0300

    reset nodemon to deploy 9

--- PI-Dogs-main/api/index.js
+++ PI-Dogs-main/api/index.js
@@ -21,7 +21,7 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 var colors = require("colors");
 const services = require("./src/services/services_API");
-const { Temperament } = require("../api/src/db.js");
+const { Temperament } = require("./src/db");
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(async () => {
